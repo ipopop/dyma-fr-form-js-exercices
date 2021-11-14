9de0eea882e3e6c3a61fb4a0ccc1e5251e721c9e
ipopop <popop@ntymail.com>
2021-11-14 16:05:29 +0100

    08 arrays - 02 objects array sort

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-export function plusGrandNombre(...args) {
-  const list = [...args]
-  let i
-  let max = -Infinity
-  for (i = 0; i < list.length; i++) {
-    if (list[i] > max) {
-      max = list[i];
-    }
-  }
-  return max;
+export function trierTableaux(arr) {
+  return arr.sort((a, b) => {
+    return b.prix - a.prix ? b.prix - a.prix : b.quantite - a.quantite;
+  });
 }
