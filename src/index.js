1d82ac09fc8e5380db583bc0ec4fd0636996ea85
ipopop <popop@ntymail.com>
2021-11-14 15:39:22 +0100

    03 ctrl structures - 02 conditional operator

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-var falsy1 = undefined;
-var falsy2 = 0;
-var falsy3 = '';
-var falsy4 = NaN;
-var falsy5 = null;
-var falsy6 = false;
+export function testFn(condition, age) {
+  // Déclarez en dessous :
+  const a = 42;
+  const b = condition ? a : 0;
+  const statut = (age >= 21) ? 'Majeur aux USA' : (age >= 18) ? 'Majeur en France' : 'Mineur';
 
-export { falsy1, falsy2, falsy3, falsy4, falsy5, falsy6 }
+  // Ne touchez pas au return.
+  return [b, statut];
+}
