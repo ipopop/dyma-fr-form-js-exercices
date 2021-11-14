91250a1b7ca681b141ad5c5715114cf75b535ac5
ipopop <popop@ntymail.com>
2021-11-14 16:06:03 +0100

    08 arrays - 03 map & filter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
-export function trierTableaux(arr) {
-  return arr.sort((a, b) => {
-    return b.prix - a.prix ? b.prix - a.prix : b.quantite - a.quantite;
-  });
+export function filtreFemmesEtAjouteNomComplet(array) {
+  return array
+    .filter((person) => person.genre === 'féminin')
+    .map((person) => ({
+      nomComplet: `${person.prenom} ${person.nom}`,
+      ...person,
+    }))
 }
+
+// const test = [
+//   { prenom: 'Albert', nom: 'Einstein', genre: 'masculin' },
+//   { prenom: 'Isaac', nom: 'Newton', genre: 'masculin' },
+//   { prenom: 'Marie', nom: 'Curie', genre: 'féminin' },
+//   { prenom: 'Max', nom: 'Planck', genre: 'masculin' },
+//   { prenom: 'Simone', nom: 'de Beauvoir', genre: 'féminin' },
+//   { prenom: 'Rosalind', nom: 'Franklin', genre: 'féminin' },
+// ]
+// filtreFemmesEtAjouteNomComplet(test)
+
+// result
+// [
+//   {
+//     prenom: 'Simone',
+//     nom: 'de Beauvoir',
+//     nomComplet: 'Simone de Beauvoir',
+//     genre: 'féminin',
+//   },
+//   {
+//     prenom: 'Rosalind',
+//     nom: 'Franklin',
+//     nomComplet: 'Rosalind Franklin',
+//     genre: 'féminin',
+//   },
+//   {
+//     prenom: 'Marie',
+//     nom: 'Curie',
+//     nomComplet: 'Marie Curie',
+//     genre: 'féminin',
+//   },
+// ]
