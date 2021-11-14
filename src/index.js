ee33bfa72230e075c865cec1888c21c09aaa78cf
ipopop <popop@ntymail.com>
2021-11-14 16:07:43 +0100

    08 arrays - 05 average calc

--- src/index.js
+++ src/index.js
@@ -1,31 +1,8 @@
-export function compterElements(arr) {
-  const result = arr.reduce(function(prev, cur) {
-    prev[cur] = (prev[cur] || 0) + 1
-    return prev
-  }, {})
-  return result
+export function obtenirMoyenne(arrays) {
+  return arrays.map(
+    (elem) =>
+      Math.round(
+        (elem.reduce((acc, curr) => acc + curr) / elem.length) * 10
+      ) / 10
+  )
 }
-
-// const testList = [
-//   'veste',
-//   'clé',
-//   'verre',
-//   'chien',
-//   'biberon',
-//   'chien',
-//   'veste',
-//   'clé',
-//   'chien',
-// ]
-
-// compterElements(testList)
-
-
-// result
-// {
-//   veste: 2,
-//   clé: 2,
-//   verre: 1,
-//   chien: 3,
-//   biberon: 1,
-// }
