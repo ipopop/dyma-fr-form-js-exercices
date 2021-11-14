67412b98b49916afda3aae89ee76ce26bb65e296
ipopop <popop@ntymail.com>
2021-11-14 16:02:06 +0100

    07 functions - 04 funct & conditions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-export function creerMultiplicateur(m) {
-  let result = n => n * m
-  return result   
+export function lePlusGrand(a, b, c) {
+  const list = [a, b, c]
+  let i
+  let max = -Infinity
+  for (i = 0; i < list.length; i++) {
+    if (list[i] > max) {
+      max = list[i];
+    }
+  }
+  return max;
 }
