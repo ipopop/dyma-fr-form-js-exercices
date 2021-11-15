c6e80d492fa24f8e690f435f46b532305d5d6b49
ipopop <popop@ntymail.com>
2021-11-15 12:59:58 +0100

    10 todo project - 07 project enhancement

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 # dyma-fr-form-js-exercices
+
+1- Install :
+
+    npm i --prefer-offline
+
+2- Start :
+
+    npm start
+
+Tree view :
+
+    ├── src
+    │   ├── index.html
+    │   ├── index.js
+    │   └── style.css
+    ├── babel.config.js
+    ├── package.json
+    └── webpack.config.js
+
+Commits reverse history :
+
+    10 todo project - 07 project enhancement
+    10 todo project - 06 edit task
+    10 todo project - 05 complete task
+    10 todo project - 04 delete task
+    10 todo project - 03 add task
+    10 todo project - 02 task display
+    10 todo project - 01 init project
+    09 DOM - 03 list modify
+    09 DOM - 02 delete DOM element
+    09 DOM - 01 modify DOM element
+    08 arrays - 06 median calc
+    08 arrays - 05 average calc
+    08 arrays - 04 reduce
+    08 arrays - 03 map & filter
+    08 arrays - 02 objects array sort
+    08 arrays - 01 the greatest number
+    07 functions - 04 funct & conditions
+    07 functions - 03 funct & closures
+    07 functions - 02 arrow funct
+    07 functions - 01 funct declaration
+    06 objects - 06 object deep copy
+    06 objects - 05 object property check
+    06 objects - 04 spread & object assign
+    06 objects - 03 assignment by decomposition
+    06 objects - 02 object & calc. property
+    06 objects - 01 declaring a first object
+    05 character strings - 04 regular exp & capturing group
+    05 character strings - 03 slice & padStart methods
+    05 character strings - 02 trim & split methods
+    05 character strings - 01 toUpperCase & toLowerCase methods
+    04 numbers - 02 random number generation
+    04 numbers - 01 round numbers to float point
+    03 ctrl structures - 04 for loop
+    03 ctrl structures - 03 switch instruction
+    03 ctrl structures - 02 conditional operator
+    03 ctrl structures - 01 falsy values
+    02 var-types & concat - 04 concatenation
+    02 var-types & concat - 03 declar diff var & types
+    02 var-types & concat - 02 declar & assign many var
+    02 var-types & concat - 01 declar & assign var
+    01 initial commit - intro - funct substract
--- src/index.js
+++ src/index.js
@@ -9,8 +9,16 @@ const input = document.querySelector('form > input')
 form.addEventListener('submit', e => {
   e.preventDefault()
   const data = input.value
+  const titleCase = data.charAt(0).toUpperCase() + data.slice(1)
   input.value = ''
-  addTodo(data)
+  addTodo(titleCase)
+})
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && todos.find(t => t.editMode)) {
+    todos.find(t => t.editMode).editMode = false
+    displayTodo()
+  }
 })
 
 const todos = [
@@ -21,8 +29,8 @@ const todos = [
 function displayTodo() {
   const todosInsert = todos.map((todo, index) => {
     return (todo.editMode) ?
-      createTodoEditElement(todo, index) :
-      createTodoElement(todo, index)
+    createTodoEditElement(todo, index) :
+    createTodoElement(todo, index)
   })
   ul.innerHTML = ''
   ul.append(...todosInsert)
@@ -32,8 +40,10 @@ function createTodoElement(todo, index) {
   const li = document.createElement('li')
   const btnDel = document.createElement('button')
   btnDel.innerHTML = 'Supprimer'
+  btnDel.classList.add('danger')
   const btnEdit = document.createElement('button')
   btnEdit.innerHTML = 'Edition'
+  btnEdit.classList.add('success')
   btnDel.addEventListener('click', e => {
     e.stopPropagation()
     deleteTodo(index)
@@ -46,8 +56,17 @@ function createTodoElement(todo, index) {
     <span class="todo ${todo.done ? 'done' : ''}"></span>
     <p>${todo.text}</p>
   `
-  li.addEventListener('click', event => {
-    toggleTodo(index)
+  let timer
+  li.addEventListener('click', e => {
+    e.stopPropagation()
+    if (e.detail === 1) {
+      timer = setTimeout(() => {
+        toggleTodo(index)
+      }, 200)
+    }else if (e.detail > 1) {
+      clearTimeout(timer)
+      toggleEditMode(index)
+    }
   })
   li.append(btnEdit, btnDel)
   return li
@@ -60,21 +79,33 @@ function createTodoEditElement(todo, index) {
   input.value = todo.text
   const btnSave = document.createElement('button')
   btnSave.innerHTML = 'Sauvegarder'
+  btnSave.classList.add('danger')
   const btnCancel = document.createElement('button')
   btnCancel.innerHTML = 'Annuler'
+  btnCancel.classList.add('success')
   btnCancel.addEventListener('click', e => {
     e.stopPropagation()
     toggleEditMode(index)
   })
   btnSave.addEventListener('click', e => {
+    e.stopPropagation()
     editTodo(index, input)
   })
+  document.addEventListener("keypress", e => {
+    e.stopPropagation();
+    if (e.key === 'Enter') {
+      btnSave.click()
+    }
+  })
   li.append(input, btnCancel, btnSave)
+  setTimeout(() => input.focus(), 0)
   return li
 }
 
 function addTodo(text) {
-  todos.push({ text, done: false })
+  (!text) ?
+    alert('todo is empty!') :
+    todos.push({ text, done: false })
   displayTodo()
 }
 
@@ -95,6 +126,10 @@ function toggleEditMode(index) {
 
 function editTodo(index, input) {
   const data = input.value
+  if (!data) {
+    alert('todo is empty!')
+    return ''
+  }
   todos[index].text = data
   todos[index].editMode = false
   displayTodo()
